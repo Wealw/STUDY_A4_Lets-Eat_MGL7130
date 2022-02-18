b418f133ede5e3b69a299718753139413c48bba2
Théo Weimann <theo.weimann.priv@gmail.com>
2022-02-17 21:58:31 -0500

    Add header & map instance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component,NgModule} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles: [`
+    agm-map {
+      height: 300px;
+    }
+  `],
 })
 export class AppComponent {
-  title = 'lets-eat';
+  title = 'Lets-Eat';
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,37 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
+import {HeaderComponent} from './header/header.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {GoogleMapsModule} from '@angular/google-maps';
+import { MainComponent } from './main/main.component';
+import { OptionComponent } from './option/option.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    OptionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    GoogleMapsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
