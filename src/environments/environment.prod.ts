34e401aeeb4495a2935bc4f714a3ee718e30ccfe
Théo Weimann <theo.weimann.priv@gmail.com>
2022-03-08 20:50:22 -0500

    Add firebase CI/CD with gitlab

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD0vBu1QQw6FffaxHqM048LdtmaPDNpic8",
+    authDomain: "letseat-bc283.firebaseapp.com",
+    projectId: "letseat-bc283",
+    storageBucket: "letseat-bc283.appspot.com",
+    messagingSenderId: "10147661197",
+    appId: "1:10147661197:web:d9218bfd3000e15451e9c4"
+  }
 };
