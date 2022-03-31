cfa8acfc5c7456e605db9ac9e12b118ccdbc9668
Wealw <theo.weimann.priv@gmail.com>
2022-03-30 23:08:31 -0400

    feat: push notification early access (v3)

--- src/app/services/messaging/messaging.service.ts
+++ src/app/services/messaging/messaging.service.ts
@@ -3,29 +3,26 @@ import { AngularFireMessaging } from '@angular/fire/compat/messaging';
 import { BehaviorSubject } from 'rxjs'
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 import {environment} from "../../../environments/environment.prod";
+import {AngularFirestore} from "@angular/fire/compat/firestore";
 @Injectable()
 export class MessagingService {
   currentMessage = new BehaviorSubject<any>(null);
-  constructor(private angularFireMessaging: AngularFireMessaging) {
+  constructor(private angularFireMessaging: AngularFireMessaging, private angularFirestore : AngularFirestore) {
   }
   requestPermission() {
     const messaging = getMessaging();
     getToken(messaging, { vapidKey: environment.firebase.vapidKey}).then(
       (currentToken) => {
         if (currentToken) {
-          console.log("Hurraaa!!! we got the token.....");
-          console.log(currentToken);
-        } else {
-          console.log('No registration token available. Request permission to generate one.');
+          // noinspection JSIgnoredPromiseFromCall
+          this.angularFirestore.collection('deviceId').doc(currentToken).set({deviceId : currentToken})
         }
-      }).catch((err) => {
-      console.log('An error occurred while retrieving token. ', err);
+      }).catch((_) => {
     });
   }
   listen() {
     const messaging = getMessaging();
     onMessage(messaging, (payload) => {
-      console.log('Message received. ', payload);
       this.currentMessage.next(payload);
     });
   }
